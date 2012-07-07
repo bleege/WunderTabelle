c8683946a6c968ec8735b71b32305f8932a9d3fc
Brad Leege <bleege@gmail.com>
2012-07-07 15:50:29 -0500

    Initial Commit

---
+++ WunderTabelleTests/WunderTabelleTests.h
@@ -0,0 +1,13 @@
+//
+//  WunderTabelleTests.h
+//  WunderTabelleTests
+//
+//  Created by Brad Leege on 7/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WunderTabelleTests : SenTestCase
+
+@end
---
+++ WunderTabelleTests/WunderTabelleTests.m
@@ -0,0 +1,32 @@
+//
+//  WunderTabelleTests.m
+//  WunderTabelleTests
+//
+//  Created by Brad Leege on 7/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WunderTabelleTests.h"
+
+@implementation WunderTabelleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WunderTabelleTests");
+}
+
+@end
